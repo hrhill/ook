62dfcca742611fffada1f95d55b7685fd6c9cfaa
user <no.email>
2013-09-03 18:39:40 +0100

    Removing isave and dsave arrays.

--- inspiration/main.c
+++ inspiration/main.c
@@ -45,7 +45,7 @@ int main(int argc, char** argv){
     double dsave[13];
     int i;
     for (i = 0; i < ntries; ++i){
-        dcsrch_(&stp, &f, &g, &ftol, &gtol, &xtol, task, &stpmin, &stpmax, isave, dsave, 255);
+        dcsrch_(&stp, &f, &g, &ftol, &gtol, &xtol, task, &stpmin, &stpmax, 255);
         if (s_cmp(task, "FG", 2, 2) == 0){
             nfev = nfev + 1;
             f = phi51(stp, b, &g);
