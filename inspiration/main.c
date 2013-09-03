1c56554f99a26ed3c04def303ddf38d15095199e
user <no.email>
2013-09-03 19:02:10 +0100

    Removing s_copy

--- inspiration/main.c
+++ inspiration/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "line_search.h"
 
@@ -40,7 +41,7 @@ int main(int argc, char** argv){
     stp = factor * stp0;
     int nfev = 0;
     char task[255];
-    s_copy(task, "START", 5, 5);
+    strcpy(task, "START");
     int isave[2];
     double dsave[13];
     int i;
