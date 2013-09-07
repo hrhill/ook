46f86e338b1bb4ac6ad0ff7bc0430a8052ce2af0
harry <hill.harry.r@gmail.com>
2013-09-07 15:33:35 +0100

    Refactoring to use test structure.

--- include/more_thuente.h
+++ include/more_thuente.h
@@ -1,14 +1,45 @@
 #ifndef OOK_MORE_THUENTE_H_
 #define OOK_MORE_THUENTE_H_
 
-template <typename Phi>
-double 
-more_thuente(Phi phi, double phi0, double dphi0, double a, double a_min, double a_max, int max_function_evals = 20)
-{
-    const double c1 = 1e-04;
-    const double c2 = 9e-01;
+#include <tuple>
+#include <iomanip>
 
-    return a;
+#include "dcsrch.h"
+
+template <typename F>
+void
+more_thuente_line_search(F phi, const double stp0, const options& opts){
+
+    double stp = 0.0;
+    double f0, g0;
+    std::tie(f0, g0) = phi(stp);
+
+    stp = stp0;
+    int nfev = 1;
+    task_value task = task_value::start;
+
+    double f, g;
+    std::tie(f, g) = phi(stp);
+    dcsrch_struct dcsrch_(f0, g0, stp, opts.stpmax - opts.stpmin);
+    do{
+        const bool sufficient_decrease = sufficient_decrease_condition(f, f0, g0, stp, opts.ftol);
+        const bool curvature = curvature_condition(g, g0, opts.gtol);
+        if (sufficient_decrease && curvature){
+            task = task_value::convergence;
+            break;
+        }
+        task = dcsrch_(stp, f, g, opts);
+        if (task == task_value::fg) {
+            nfev = nfev + 1;
+            std::tie(f, g) = phi(stp);
+        }
+    }while (true);
+    std::cout << std::scientific 
+              << std::setw(16) << stp0 
+              << std::setw(16) << task 
+              << std::setw(4) << nfev 
+              << std::setw(16) << stp  
+              << std::setw(16) << g << std::endl;    
 }
 
 #endif
