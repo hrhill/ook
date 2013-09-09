383a16fa8f3ecbc20354ace6d41bd87bda713b10
harry <hill.harry.r@gmail.com>
2013-09-09 11:24:55 +0100

    pushing up weekends mods.

--- include/more_thuente.h
+++ include/more_thuente.h
@@ -16,7 +16,6 @@ more_thuente_line_search(F phi, const double stp0, const options& opts){
 
     stp = stp0;
     int nfev = 1;
-    task_value task = task_value::start;
 
     double f, g;
     std::tie(f, g) = phi(stp);
@@ -24,19 +23,17 @@ more_thuente_line_search(F phi, const double stp0, const options& opts){
     do{
         const bool sufficient_decrease = sufficient_decrease_condition(f, f0, g0, stp, opts.ftol);
         const bool curvature = curvature_condition(g, g0, opts.gtol);
+
         if (sufficient_decrease && curvature){
-            task = task_value::convergence;
             break;
         }
-        task = dcsrch_(stp, f, g, opts);
-        if (task == task_value::fg) {
-            nfev = nfev + 1;
-            std::tie(f, g) = phi(stp);
-        }
+        dcsrch_(stp, f, g, opts);
+        nfev = nfev + 1;
+        std::tie(f, g) = phi(stp);
     }while (true);
     std::cout << std::scientific 
               << std::setw(16) << stp0 
-              << std::setw(16) << task 
+              << std::setw(16) << "CONVERGENCE" 
               << std::setw(4) << nfev 
               << std::setw(16) << stp  
               << std::setw(16) << g << std::endl;    
