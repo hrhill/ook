dc4d805c77bbbadd5e98d0892491c3df2f8adb03
harry <hill.harry.r@gmail.com>
2013-09-11 18:59:30 +0100

    Haskell code generation mkI.

--- tests/GenerateTestFunctions.hs
+++ tests/GenerateTestFunctions.hs
@@ -1,35 +1,62 @@
-import TestHeader 
+import Data.Char (toUpper)
+import Control.Monad (forM_)
+import Data.String.Utils (replace)
 
+import TestHeader (templateText)
 
 data Function = Function{
     name :: String,
-    n :: Int,
-    m :: Int,
+    n :: String,
+    m :: String,
     x0 :: String,
     minima :: String,
-    fmin :: Double,
+    fmin :: String,
+    tol :: String,
     body :: String
 } deriving (Show)
 
+epsilon = "std::numeric_limits<double>::epsilon()"
 
-rosenbrockBody = "const real_type f1 = 10.0 * (x(1) - x(0) * x(0));\n\
-                 \const real_type f2 = 1.0 - x(0);\n\
-                 \const real_type f = f1 * f1 + f2 * f2;\n\
-                 \vector_type df(2);\n\
-                 \df(0) = -4.0 * 10.0 * f1 * x(0) - 2.0 * f2;\n\
-                 \df(1) = -2.0 * 10.0 * f1;\n\
-                 \return std::make_pair(f, df);\n"
-rosenbrock = Function "rosenbrock" 2 2 "{-1.2, 1}" "{1.0, 1.0}" 0.0 rosenbrockBody
+rosenbrockBody = "    const real_type f1 = 10.0 * (x(1) - x(0) * x(0));\n\
+                 \    const real_type f2 = 1.0 - x(0);\n\
+                 \    f = f1 * f1 + f2 * f2;\n\
+                 \    df(0) = -4.0 * 10.0 * f1 * x(0) - 2.0 * f2;\n\
+                 \    df(1) = -2.0 * 10.0 * f1;\n"
+
+freudensteinrothBody = "    const real_type x1 = x(0);\n\
+                        \    const real_type x2 = x(1);\n\
+                        \    const real_type f1 = -13 + x1 + ((5 - x2) * x2 - 2) * x2;\n\
+                        \    const real_type f2 = -29 + x1 + ((x2 + 1) * x2 - 14) * x2;\n\
+                        \    f = f1 * f1 + f2 * f2;\n\
+                        \    df(0) = 2 * f1 + 2 * f2;\n\
+                        \    df(1) = 2 * f1 * (10 * x2 - 3 * x2 * x2 - 2)\n\
+                        \              + 2 * f2 * ( 3 * x2 * x2 + 2 * x2 - 14);\n"
+
+rosenbrock = Function "rosenbrock" "2" "2" "{-1.2, 1}" "{1.0, 1.0}" "0.0" epsilon rosenbrockBody
+freudenstein_roth = Function "freudenstein_roth" "2" "2" "{0.5, 2}" "{5.0, 4.0}" "0.0" "1e-04" freudensteinrothBody
 
 functionList = [rosenbrock]
 
-replaceTokens :: String -> Function -> (String, String)
-replaceTokens s f = (filename, contents) where
-                    filename = name f
-                    contents = s
+upperCase = map toUpper
+
+replaceIfDef = replace "@IFDEF@" . upperCase . name
+replaceName = replace "@NAME@" . name
+replaceN = replace "@N@" . n
+replaceM = replace "@M@" . m
+replaceX0 = replace "@XO@" . x0 
+replaceMinima = replace "@MINIMA@" . minima
+replaceFMin = replace "@FMIN@" . fmin
+replaceTol = replace "@TOL@" . fmin
+replaceBody = replace "@BODY@" . body
+
+doReplacements f = (replaceIfDef f . replaceBody f . replaceFMin f 
+                        . replaceMinima f . replaceX0 f . replaceTol f
+                            . replaceM f . replaceN f . replaceName f)
+
+generateHeader f = writeFile filename filecontents
+                    where
+                        filename = "./test_functions/" ++ name f ++ ".h" 
+                        filecontents = (doReplacements f) templateText
 
 main :: IO ()
-main = do
-    let results = map (replaceTokens templateText) functionList 
---    putStrLn show results
-    putStrLn templateText
+main = forM_ functionList generateHeader 
--- tests/TestHeader.hs
+++ tests/TestHeader.hs
@@ -2,45 +2,48 @@ module TestHeader where
 
 templateText :: String
 templateText = 
-    "#ifndef IFDEF_H_\n \
-    \#define IFDEF_H_\n \
-    \\n \
-    \#include <vector>\n \
-    \\n \
-    \template <typename Vector>\n \
-    \struct NAME\n \
-    \{\n \
-    \    typedef Vector vector_type;\n \
-    \    typedef typename vector_type::value_type real_type;\n \
-    \\n \
-    \    std::tuple<real_type, vector_type>\n \
-    \    operator()(const vector_type& x) const\n \
-    \    {\n \
-    \        BODY\n \
-    \    }\n \
-    \\n \
-    \    static const int n = N;\n \
-    \    static const int m = M;\n \
-    \    static real_type f_min;\n \
-    \    static real_type tolerance;\n \
-    \    static std::vector<real_type> minima;\n \
-    \    static std::vector<real_type> x0;\n \
-    \};\n \
-    \\n \
-    \template <typename Vector>\n \
-    \typename Vector::value_type\n \
-    \NAME<Vector>::f_min = FMIN;\n \
-    \\n \
-    \template <typename Vector>\n \
-    \typename Vector::value_type\n \
-    \NAME<Vector>::tolerance = TOL;\n \
-    \\n \
-    \template <typename Vector>\n \
-    \std::vector<typename Vector::value_type>\n \
-    \NAME<Vector>::minima = MINIMA;\n \
-    \\n \
-    \template <typename Vector>\n \
-    \std::vector<typename Vector::value_type>\n \
-    \NAME<Vector>::x0 = XO;\n \
-    \\n \
+    "#ifndef @IFDEF@_H_\n\
+    \#define @IFDEF@_H_\n\
+    \\n\
+    \#include <vector>\n\
+    \\n\
+    \template <typename Vector>\n\
+    \struct @NAME@\n\
+    \{\n\
+    \    typedef Vector vector_type;\n\
+    \    typedef typename vector_type::value_type real_type;\n\
+    \\n\
+    \    std::tuple<real_type, vector_type>\n\
+    \    operator()(const vector_type& x) const\n\
+    \    {\n\
+    \        real_type f(0.0);\n\
+    \        vector_type df(@N@, 0.0);
+    \        @BODY@\n\
+    \        return std::make_pair(f, df);\n
+    \    }\n\
+    \\n\
+    \    static const int n = @N@;\n\
+    \    static const int m = @M@;\n\
+    \    static real_type f_min;\n\
+    \    static real_type tolerance;\n\
+    \    static std::vector<real_type> minima;\n\
+    \    static std::vector<real_type> x0;\n\
+    \};\n\
+    \\n\
+    \template <typename Vector>\n\
+    \typename Vector::value_type\n\
+    \@NAME@<Vector>::f_min = @FMIN@;\n\
+    \\n\
+    \template <typename Vector>\n\
+    \typename Vector::value_type\n\
+    \@NAME@<Vector>::tolerance = @TOL@;\n\
+    \\n\
+    \template <typename Vector>\n\
+    \std::vector<typename Vector::value_type>\n\
+    \@NAME@<Vector>::minima = @MINIMA@;\n\
+    \\n\
+    \template <typename Vector>\n\
+    \std::vector<typename Vector::value_type>\n\
+    \@NAME@<Vector>::x0 = @XO@;\n\
+    \\n\
     \#endif\n"
