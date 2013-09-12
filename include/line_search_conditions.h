9ef9a944f7ed9565695a10398645f3527be8eca4
user <no.email>
2013-09-12 18:04:19 +0100

    Removing state from line search.

--- include/line_search_conditions.h
+++ include/line_search_conditions.h
@@ -6,63 +6,29 @@
 
 namespace ook{
 
-template <typename State>
+template <typename T>
 bool
-sufficient_decrease_condition(const State& s, const typename State::real_type& c)
+sufficient_decrease_condition(const T& fx, const T& fxap, const T& dfx_dot_p, const T& a, const T& c)
 {
-    return s.fxap <= s.fx + c * s.a * s.dfx_dot_p;    
+    assert(T(0.0) < c && c < T(1.0));
+    return fxap <= fx + c * a * dfx_dot_p;    
 }
 
-template <typename State>
+template <typename T>
 bool
-curvature_condition(const State& s, const typename State::real_type& c)
+curvature_condition(const T& dfx_dot_p, const T& dfxap_dot_p, const T& c)
 {
-    return s.dfxap_dot_p <= c * s.dfx_dot_p;    
+    assert(T(0.0) < c && c < T(1.0));
+    return fabs(dfxap_dot_p) <= c * fabs(dfx_dot_p);    
 }
 
-template <typename State>
+template <typename T>
 bool
-auxilliary_curvature_condition(const State& s, const typename State::real_type& c)
+strong_wolfe_conditions(const T& fx, const T& fxap, const T& dfx_dot_p, const T& dfxap_dot_p, const T& a,
+                 const T& c1 = T(1e-04), const T& c2 = T(9e-01))
 {
-    return fabs(s.dfxap_dot_p) <= c * fabs(s.dfx_dot_p);    
-}
-
-template <typename State>
-bool
-step_length_control(const State& s, const typename State::real_type& c)
-{
-    return s.fx + (1.0 - c) * s.a * s.dfx_dot_p;
-}
-
-template <typename State>
-bool
-wolfe_conditions(const State& s, 
-                 const typename State::real_type& c1 = typename State::real_type(1e-04),
-                 const typename State::real_type& c2 = typename State::real_type(9e-01))
-{
-    typedef typename State::real_type real_type;
-    assert(real_type(0.0) < c1 && c1 < c2 && c2 < real_type(1.0));
-    return sufficient_decrease_condition(s, c1) && curvature_condition(s, c2);
-}
-
-template <typename State>
-bool
-strong_wolfe_conditions(const State& s, 
-                 const typename State::real_type& c1 = typename State::real_type(1e-04),
-                 const typename State::real_type& c2 = typename State::real_type(9e-01))
-{
-    typedef typename State::real_type real_type;
-    assert(real_type(0.0) < c1 && c1 <= c2 && c2 < real_type(1.0));
-    return sufficient_decrease_condition(s, c1) && auxilliary_curvature_condition(s, c2);
-}
-
-template<typename State>
-bool
-goldstein_conditions(const State& s, const typename State::real_type& c = typename State::real_type(0.25))
-{
-    typedef typename State::real_type real_type;
-    assert(c > real_type(0.0) && c < real_type(1.0));
-    return sufficient_decrease_condition(s, c) && step_length_control(s, c);
+    return sufficient_decrease_condition(fx, fxap, dfx_dot_p, a, c1) 
+            && curvature_condition(dfx_dot_p, dfxap_dot_p, c2);
 }
 
 } // ns ook
--- include/state.h
+++ include/state.h
@@ -4,45 +4,16 @@
 #include <iostream>
 #include <boost/numeric/ublas/vector.hpp>
 
+#include "state_value.h"
+
 namespace ook{
-
-enum class state_value{
-    start,
-    update,
-    warning_rounding_error_prevents_progress,
-    warning_xtol_satisfied,
-    warning_stp_eq_stpmax,
-    warning_stp_eq_stpmin,
-    convergence
-};
-
-const char* state_value_string[] = {"start",
-    "update",
-    "warning_rounding_error_prevents_progress",
-    "warning_xtol_satistfied",
-    "warning_stp_eq_stpmax",
-    "warning_stp_eq_stpmin",
-    "convergence"}; 
-
-std::ostream&
-operator<<(std::ostream& os, const state_value& tv)
-{
-    auto id = static_cast<std::underlying_type<state_value>::type>(tv);
-   return os << std::string(state_value_string[id]);
-}
 
 struct state{
     typedef double real_type;
 
-    state(double fx_, double fxap_, double dfx_dot_p_, double dfxap_dot_p_, double a_, state_value value_, int nfev_ = 0)
+    state()
     : 
-        fx(fx_), 
-        fxap(fxap_), 
-        dfx_dot_p(dfx_dot_p_),
-        dfxap_dot_p(dfxap_dot_p_),
-        a(a_),
-        value(value_),
-        nfev(nfev_)
+        nfev(0)
     {}
 
     double fx;
