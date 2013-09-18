6d1d6fa8f34c1482b2568df004770b34c83ab3db
harry <hill.harry.r@gmail.com>
2013-09-18 22:14:37 +0100

    Adding FindAtlas.

--- tests/cmake_install.cmake
+++ tests/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/harry/projects/ook/tests
+# Install script for directory: /home/harry/projects/libraries/ook/tests
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,8 +34,8 @@ ENDIF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
 
 IF(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  INCLUDE("/home/harry/projects/ook/tests/line_search/cmake_install.cmake")
-  INCLUDE("/home/harry/projects/ook/tests/finite_differences/cmake_install.cmake")
+  INCLUDE("/home/harry/projects/libraries/ook/tests/line_search/cmake_install.cmake")
+  INCLUDE("/home/harry/projects/libraries/ook/tests/finite_differences/cmake_install.cmake")
 
 ENDIF(NOT CMAKE_INSTALL_LOCAL_ONLY)
 
