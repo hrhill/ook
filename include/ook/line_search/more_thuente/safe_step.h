bc094b065189cc822243a28dd78d8e52230c77fd
harry <hill.harry.r@gmail.com>
2014-02-27 14:46:09 +0000

    Refactor step code.

--- include/ook/line_search/more_thuente/safe_step.h
+++ include/ook/line_search/more_thuente/safe_step.h
@@ -10,6 +10,57 @@ namespace ook{
 
 namespace line_search {
 
+/// \brief Sign function
+template <typename T>
+inline
+T
+signum(T x)
+{
+	return copysign(1.0, x);
+}
+
+/// \brief Calculate the secant step.
+template <typename T>
+inline
+T
+secant_step(T x, T dx, T y, T dy)
+{
+	return y + dy / (dy - dx) * (x - y);
+}
+
+/// \brief Calculate the value which minimizes the quadratic
+/// interpolant to the inputs.
+template <typename T>
+inline
+T
+quadratic_step(T x, T fx, T dx, T y, T fy)
+{
+	return  x + dx / ((fx - fy) / (y - x) + dx) / 2.0 * (y - x);
+}
+
+/// \brief Calculate the value which minimizes the cubic
+/// interpolant to the inputs.
+template <typename T>
+inline
+T
+cubic_step(T x, T fx, T dx, T y, T fy, T dy)
+{
+	const T d1 = dx + dy - 3 * (fx - fy)/(x - y);
+	const T s = std::max({fabs(d1), fabs(dx), fabs(dy)});
+	const T d2 = signum(y - x) * s * sqrt(pow(d1 / s, 2) - (dx / s) * (dy / s));
+	return y - (y - x) * (dy + d2 - d1)/(d2 + dy - dx + d2);
+}
+
+/// \brief Return if a is closer to c than b.
+template <typename T>
+inline
+bool
+is_closer(T a, T b, T c)
+{
+	return fabs(a - c) < fabs(b - c);
+}
+
+
 template <typename T>
 T
 cubic_minimizer(T f1, T f2, T d1, T d2, T st1, T st2)
@@ -28,15 +79,12 @@ inline
 std::pair<T, bool>
 case1(T fx, T dx, T stx, T fp, T dp, T stp)
 {
-	T stpf;
-	const T r = cubic_minimizer(fx, fp, dx, dp, stx, stp);
-	const T stpc = stx + r * (stp - stx);
-	const T stpq = stx + (dx / ((fx - fp) / (stp - stx) + dx) / T(2.0)) * (stp - stx);
-	if (fabs(stpc - stx) < fabs(stpq - stx)){
-	    stpf = stpc;
-	} else {
-	    stpf = stpc + T(0.5) * (stpq - stpc);
-	}
+	const T stpc = cubic_step(stx, fx, dx, stp, fp, dp);
+	const T stpq = quadratic_step(stx, fx, dx, stp, fp);
+ 	// If the cubic step is closer to stx than the quadratic step, the
+    // cubic step is taken, otherwise the average of the cubic and
+    // quadratic steps is taken.
+	T stpf = is_closer(stpc, stpq, stx) ? stpc : (stpq + stpc) / 2;
 	return std::make_pair(stpf, true);
 }
 
@@ -45,16 +93,13 @@ inline
 std::pair<T, bool>
 case2(T fx, T dx, T stx, T fp, T dp, T stp)
 {
-	T stpf;
-	const T r = cubic_minimizer(fp, fx, dp, dx, stp, stx);
-	const T stpc = stp + r * (stx - stp);
-	const T stpq = stp + dp / (dp - dx) * (stx - stp);
-	if (fabs(stpc - stp) > fabs(stpq - stp)){
-		stpf = stpc;
-	} else {
-		stpf = stpq;
-	}
-	return std::make_pair(stpf, true);	
+    const T stpc = cubic_step(stx, fx, dx, stp, fp, dp);
+	const T stpq = secant_step(stx, dx, stp, dp);
+	// If the cubic step is farther from
+    // stp than the secant step, the cubic step is taken, otherwise the
+    // secant step is taken.
+    T stpf = is_closer(stpc, stpq, stp) ? stpq : stpc;
+	return std::make_pair(stpf, true);
 }
 
 template <typename T>
@@ -62,14 +107,14 @@ inline
 T
 case3(T stx, T fx, T dx, T sty, T fy, T dy, T stp, T fp, T dp, bool brackt, T stpmin, T stpmax)
 {
-	/* 	The cubic step is computed only if the cubic tends to infinity
-	in the direction of the step or if the minimum of the cubic
-	is beyond stp. Otherwise the cubic step is defined to be the
-	secant step. */
-	const T theta = 3.0 * (fx - fp) / (stp - stx) + dx + dp;
-	/* The case gamma = 0 only arises if the cubic does not tend
-	to infinity in the direction of the step.*/
+	// The cubic step is computed only if the cubic tends to infinity
+	// in the direction of the step or if the minimum of the cubic
+	// is beyond stp. Otherwise the cubic step is defined to be the
+	// secant step.
+	const T theta = T(3.0) * (fx - fp) / (stp - stx) + dx + dp;
 	const T s = std::max({fabs(theta), fabs(dx), fabs(dp)});
+	// The case gamma = 0 only arises if the cubic does not tend
+	// to infinity in the direction of the step.*/
 	T gamma = s * sqrt(std::max(T(0.0), std::pow(theta/s, 2) - dx / s * dp / s));
 	if (stx < stp){
 		gamma = -gamma;
@@ -77,148 +122,107 @@ case3(T stx, T fx, T dx, T sty, T fy, T dy, T stp, T fp, T dp, bool brackt, T st
 	const T p = gamma - dp + theta;
 	const T q = gamma - dp + gamma + dx;
 	const T r = p / q;
-	
+
 	T stpc = (stp > stx) ? stpmax : stpmin;
 	if (r < T(0.0) && gamma != T(0.0)) {
 	    stpc = stp + r * (stx - stp);
 	}
 	T stpf;
-	const T delta(0.66); // must be less than 1.	
+	const T delta(0.66); // must be less than 1.
 	const T stpq = stp + dp / (dp - dx) * (stx - stp);
 	if (brackt){
-		/* A minimizer has been bracketed. If the cubic step is
-		closer to stp than the secant step, the cubic step is
-		taken, otherwise the secant step is taken. */
-	    if (fabs(stpc - stp) < fabs(stpq - stp)) {
-			stpf = stpc;
-	    } else {
-			stpf = stpq;
-	    }
+		// A minimizer has been bracketed. If the cubic step is
+		// closer to stp than the secant step, the cubic step is
+		// taken, otherwise the secant step is taken.
+		stpf = is_closer(stpc, stpq, stp) ? stpc : stpq;
 	    if (stp > stx) {
 	    	stpf = std::min(stp + delta * (sty - stp), stpf);
 	    } else {
 			stpf = std::max(stp + delta * (sty - stp), stpf);
 	    }
 	} else {
-		/* A minimizer has not been bracketed. If the cubic step is
-		farther from stp than the secant step, the cubic step is
-		taken, otherwise the secant step is taken. */
-	    if (fabs(stpc - stp) > fabs(stpq - stp)) {
-	    	stpf = stpc;
-	    } else {
-	    	stpf = stpq;
-	    }
+		// A minimizer has not been bracketed. If the cubic step is
+		// farther from stp than the secant step, the cubic step is
+		// taken, otherwise the secant step is taken.
+		stpf = !is_closer(stpc, stpq, stp) ? stpc : stpq;
 	    stpf = std::min(stpmax, stpf);
 	    stpf = std::max(stpmin, stpf);
 	}
 	return stpf;
 }
 
-template <typename T>
-T
-case4(T stx, T sty, T fy, T dy, T stp, T fp, T dp, bool brackt, T stpmin, T stpmax)
-{
-	T stpf = (stp > stx) ? stpmax : stpmin;
-	if (brackt) {
-	    const T r = cubic_minimizer(fp, fy, dp, dy, stp, sty);
-	    stpf = stp + r * (sty - stp);
-	}
-	return stpf;
-}
 
+// This function computes a safeguarded step for a search
+// procedure and updates an interval that contains a step that
+// satisfies a sufficient decrease and a curvature condition.
+
+// The parameter stx contains the step with the least function
+// value. If brackt == true then a minimizer has
+// been bracketed in an interval with endpoints stx and sty.
+// The parameter stp contains the current step.
+// The function assumes that if brackt is set to true. then
+
+//       min(stx,sty) < stp < max(stx,sty),
+
+// and that the derivative at stx is negative in the direction
+// of the step.
+
+// stx is the best step obtained so far and is an endpoint of
+// the interval that contains the minimizer.
+
+//   fx is the function at stx.
+//   dx is the derivative of the function at stx.
+//        The derivative must be negative in the direction of
+//        the step, that is, dx and stp - stx must have opposite
+//        signs.
+
+//   sty is the second endpoint of the interval that contains the minimizer.
+//   fy is the function at sty.
+//   dy is the derivative of the function at sty.
+
+//   stp is the current step. If brackt is set to true
+//        then on input stp must be between stx and sty.
+//   fp is is the function at stp.
+//   dp is the the derivative of the function at stp.
+
+//   brackt is an bool variable which specifies if a minimizer has been
+//   bracketed.
+//   stpmin is a lower bound for the step.
+//   stpmax is an upper bound for the step.
 template <typename T>
 T
 safe_step(T& stx, T& fx, T& dx, T& sty, T& fy, T& dy, const T& stp, const T& fp, const T& dp, bool& brackt, const T& stpmin, const T& stpmax)
 {
-/*	This subroutine computes a safeguarded step for a search
-	procedure and updates an interval that contains a step that
-	satisfies a sufficient decrease and a curvature condition.
-
-	The parameter stx contains the step with the least function
-	value. If brackt is set to .true. then a minimizer has
-	been bracketed in an interval with endpoints stx and sty.
-	The parameter stp contains the current step.
-	The subroutine assumes that if brackt is set to .true. then 
-
-		min(stx,sty) < stp < max(stx,sty),
-
-	and that the derivative at stx is negative in the direction
-	of the step.
-
-	stx is a T precision variable.
-		On entry stx is the best step obtained so far and is an
-			endpoint of the interval that contains the minimizer.
-		On exit stx is the updated best step.
-
-	fx is a T precision variable.
-		On entry fx is the function at stx.
-		On exit fx is the function at stx.
-
-	dx is a T precision variable.
-		On entry dx is the derivative of the function at
-			stx. The derivative must be negative in the direction of
-			the step, that is, dx and stp - stx must have opposite signs.
-		On exit dx is the derivative of the function at stx.
-
-	sty is a T precision variable.
-		On entry sty is the second endpoint of the interval that contains 
-			the minimizer.
-		On exit sty is the updated endpoint of the interval that contains 
-			the minimizer.
-
-	fy is a T precision variable.
-		On entry fy is the function at sty.
-		On exit fy is the function at sty.
-
-	dy is a T precision variable.
-		On entry dy is the derivative of the function at sty.
-		On exit dy is the derivative of the function at the exit sty.
-
-	stp is a T precision variable.
-		On entry stp is the current step. If brackt is set to .true.
-			then on input stp must be between stx and sty.
-		On exit stp is a new trial step.
-
-	fp is the function at stp
-
-	dp is the derivative of the function at stp.
-
-	brackt is an bool variable.
-		On entry brackt specifies if a minimizer has been bracketed.
-			Initially brackt must be set to .false.
-		On exit brackt specifies if a minimizer has been bracketed.
-			When a minimizer is bracketed brackt is set to .true.
-
-	stpmin is a lower bound for the step.
-
-	stpmax is an upper bound for the step.
-*/
     T stpf;
     const T sgnd = dp * (dx / fabs(dx));
-	/*	First case: A higher function value. The minimum is bracketed.
-	If the cubic step is closer to stx than the quadratic step, the
-	cubic step is taken, otherwise the average of the cubic and 
-	quadratic steps is taken. */
+	// First case: A higher function value. The minimum is bracketed.
+	// If the cubic step is closer to stx than the quadratic step, the
+	// cubic step is taken, otherwise the average of the cubic and
+	// quadratic steps is taken.
     if (fp > fx) {
 		std::tie(stpf, brackt) = case1(fx, dx, stx, fp, dp, stp);
-	/*	Second case: A lower function value and derivatives of opposite
-	sign. The minimum is bracketed. If the cubic step is farther from
-	stp than the secant step, the cubic step is taken, otherwise the
-	secant step is taken. */
+	// Second case: A lower function value and derivatives of opposite
+	// sign. The minimum is bracketed. If the cubic step is farther from
+	// stp than the secant step, the cubic step is taken, otherwise the
+	// secant step is taken.
     } else if (sgnd < 0.0) {
 		std::tie(stpf, brackt) = case2(fx, dx, stx, fp, dp, stp);
-	/* Third case: A lower function value, derivatives of the same sign,
-	and the magnitude of the derivative decreases. */
+	// Third case: A lower function value, derivatives of the same sign,
+	// and the magnitude of the derivative decreases.
     } else if (fabs(dp) < fabs(dx)) {
 		stpf = case3(stx, fx, dx, sty, fy, dy, stp, fp, dp, brackt, stpmin, stpmax);
-	/* Fourth case: A lower function value, derivatives of the same sign,
-	and the magnitude of the derivative does not decrease. If the
-	minimum is not bracketed, the step is either stpmin or stpmax,
-	otherwise the cubic step is taken. */
+	// Fourth case: A lower function value, derivatives of the same sign,
+	// and the magnitude of the derivative does not decrease. If the
+	// minimum is not bracketed, the step is either stpmin or stpmax,
+	// otherwise the cubic step is taken.
     } else {
-    	stpf = case4(stx, sty, fy, dy, stp, fp, dp, brackt, stpmin, stpmax);
+		if (brackt) {
+	    	stpf = cubic_step(sty, fy, dy, stp, fp, dp);
+		} else{
+            stpf = (stp > stx) ? stpmax : stpmin;
+		}
     }
-	/* Update the interval which contains a minimizer. */
+	// Update the interval which contains a minimizer.
     if (fp > fx) {
 		sty = stp;
 		fy = fp;
@@ -232,7 +236,7 @@ safe_step(T& stx, T& fx, T& dx, T& sty, T& fy, T& dy, const T& stp, const T& fp,
 		stx = stp;
 		fx = fp;
 		dx = dp;
-    }  
+    }
     return stpf;
 }
 
