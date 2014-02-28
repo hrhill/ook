da0ef768b02cd54099322c77d9c88cb0f5aea488
Harry Hill <harry.hill@smartodds.co.uk>
2014-02-28 18:04:37 +0000

    Simplify norm calcs.

--- include/ook/norms.h
+++ include/ook/norms.h
@@ -6,49 +6,37 @@
 
 namespace ook{
 
-namespace detail{
-template <typename T, typename Accumulator, typename Reduce>
-typename T::value_type
-norm(const T& x, Accumulator acc, Reduce reduce){
-    typedef typename T::value_type real_type;
-    return reduce(std::accumulate(x.begin(), x.end(), real_type(0.0), acc));
-}
-}
-
 /// \brief \f$ l_1 \f$ norm.
 template <typename T>
 typename T::value_type
 norm_1(const T& x){
-    typedef typename T::value_type real_type;   
-    auto acc = [](const real_type s, const real_type xi){
-                        return s + sqrt(xi * xi);   
-                };
-    auto reduce = [](const real_type& x){   
-                        return x;   
-                    };
-    return detail::norm(x, acc, reduce);
+    T r(0.0);
+    for (const auto& xi : x)
+        r += fabs(xi);
+    return r;
 }
 
 /// \brief \f$ l_2  \f$ norm.
 template <typename T>
 typename T::value_type
 norm_2(const T& x){
-    typedef typename T::value_type real_type;   
-    auto acc = [](const real_type s, const real_type xi){
-                        return s + xi * xi; 
-                };
-    return detail::norm(x, acc, sqrt);
+    T r(0.0);
+    for (const auto& xi : x)
+        r += xi * xi;
+    return sqrt(r);
 }
 
 /// \brief \f$ l_{\infty} \f$ norm.
 template <typename T>
 typename T::value_type
 norm_infinity(const T& x){
-    typedef typename T::value_type real_type;
-    return fabs(*std::max_element(x.begin(), x.end(),
-                    [](const real_type& xi, const real_type& yi){
-                        return fabs(xi) < fabs(yi);
-                    }));
+    T r(0.0);
+    for (const auto& xi : x){
+        const T fxi = fabs(xi);
+        if(fxi > r)
+            r = fxi;
+    }
+    return r;
 }
 
 }
