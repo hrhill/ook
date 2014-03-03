aafd1a888836d2538bde73ec5e07b7fcd08721cd
Harry Hill <harry.hill@smartodds.co.uk>
2014-03-03 11:09:00 +0000

    Separate call_selector from line search code.

---
+++ include/ook/call_selector.h
@@ -0,0 +1,45 @@
+#ifndef OOK_CALL_SELECTOR_H_
+#define OOK_CALL_SELECTOR_H_
+
+#include <tuple>
+
+namespace ook{
+namespace detail{
+
+// Meta function to select the right function call
+// based on the properties of the return type.
+template <typename F, typename X, typename State, int dim>
+struct call_selector{};
+
+template <typename F, typename X, typename State>
+struct call_selector<F, X, State, 1>{
+    static
+    void
+    call(F f, const X& x, State& s){
+        std::tie(s.fx) = f(x);
+    }
+};
+
+template <typename F, typename X, typename State>
+struct call_selector<F, X, State, 2>{
+    static
+    void
+    call(F f, const X& x, State& s){
+        std::tie(s.fx, s.dfx) = f(x);
+    }
+};
+
+template <typename F, typename X, typename State>
+struct call_selector<F, X, State, 3>{
+    static
+    void
+    call(F f, const X& x, State& s){
+        std::tie(s.fx, s.dfx, s.H) =  f(x);
+    }
+};
+
+} // ns detail
+
+} // ns ook
+
+#endif
