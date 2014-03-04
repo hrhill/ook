b80228b2d3b9802625776300c2a039df7b8cb466
harry <hill.harry.r@gmail.com>
2014-03-04 12:11:15 +0000

    Refactor of more_thuente.

--- include/ook/line_search/more_thuente.h
+++ include/ook/line_search/more_thuente.h
@@ -25,49 +25,17 @@ struct more_thuente{
         if (dphi0 > T(0.0)) {
             return std::make_tuple(message::search_direction_is_not_a_descent_direction, 0, phi0, dphi0);
         }
-        /*
-        bool brackt;
-        int stage;
-        double gx;
-        double gy;
-        double fx;
-        double fy;
-        double stx;
-        double sty;
-        double stmin;
-        double stmax;
-        double width;
-        double width1;
-        */
         T phia, dphia;
         std::tie(phia, dphia) = phi(a);
-        detail::state s = {false, 1, dphi0, dphia, phi0, phia, 0, a, 0, 5 * a,
-                            (opts.stpmax - opts.stpmin),
-                            (opts.stpmax - opts.stpmin) / 0.5};
+        const T width = opts.stpmax - opts.stpmin;
+        detail::search<T> search(0.0, phi0, dphi0, a, phia, dphia, 0.0, 5.0 * a, width, 2.0 * width);
+        message msg;
         while(true){
-            // Test for convergence.
-            const bool sufficient_decrease = sufficient_decrease_condition(phia, phi0, opts.ftol, a, dphi0);
-            const bool curvature = curvature_condition(dphia, opts.gtol, dphi0);
-
-            if (sufficient_decrease && curvature)
-                break;
-            // Test for warnings.
-            if (s.brackt && (a <= s.stmin || a >= s.stmax)) {
-                return std::make_tuple(message::warning_rounding_error_prevents_progress, a, phia, dphia);
-            }
-            if (s.brackt && s.stmax - s.stmin <= opts.xtol * s.stmax) {
-                return std::make_tuple(message::warning_xtol_satisfied, a, phia, dphia);
-            }
-            if (a == opts.stpmax && sufficient_decrease && curvature) {
-                return std::make_tuple(message::warning_stp_eq_stpmax, a, phia, dphia);
-            }
-            if (a == opts.stpmin && (!sufficient_decrease || !curvature)) {
-                return std::make_tuple(message::warning_stp_eq_stpmin, a, phia, dphia);
-            }
-            a = detail::search(a, phia, dphia, phi0, dphi0, opts, s);
+            std::tie(msg, a) = search(a, phia, dphia, opts);
+            if (msg != message::update) break;
             std::tie(phia, dphia) = phi(a);
         }
-        return std::make_tuple(message::convergence, a, phia, dphia);
+        return std::make_tuple(msg, a, phia, dphia);
     }
 };
 
