641c1fee724d09df3f77e11963beaf34af812021
Harry Hill <harry.hill@smartodds.co.uk>
2014-06-13 15:47:17 +0100

    Update tools.hpp.

--- include/linear_algebra/factorisations/tools.hpp
+++ include/linear_algebra/factorisations/tools.hpp
@@ -3,6 +3,8 @@
 
 #include <algorithm>
 
+#include "linear_algebra/operations.hpp"
+
 namespace linalg{
 namespace factorisations{
 namespace tools{
@@ -10,10 +12,10 @@ namespace tools{
 ///\brief Select the lower triangular part of the matrix.
 template <typename Matrix>
 Matrix
-select_lower_triangular(const Matrix& m){
-
-    const int nrows = m.size1();
-    const int ncols = m.size2();
+select_lower_triangular(const Matrix& m)
+{
+    const int nrows = linalg::num_rows(m);
+    const int ncols = linalg::num_cols(m);
 
     const int dim = std::min(nrows, ncols);
     const int row_offset = std::max(0, nrows - ncols);
@@ -30,10 +32,10 @@ select_lower_triangular(const Matrix& m){
 /// \brief Select the upper triangular part of the matrix.
 template <typename Matrix>
 Matrix
-select_upper_triangular(const Matrix& m){
-
-    const int nrows = m.size1();
-    const int ncols = m.size2();
+select_upper_triangular(const Matrix& m)
+{
+    const int nrows = linalg::num_rows(m);
+    const int ncols = linalg::num_cols(m);
 
     const int dim = std::min(nrows, ncols);
     const int col_offset = std::max(0, ncols - nrows);
@@ -49,21 +51,18 @@ select_upper_triangular(const Matrix& m){
 
 /// \brief Get the value of maximum magnitude on the diagonal of the matrix.
 template <typename Matrix>
-std::tuple<typename Matrix::size_type, typename Matrix::value_type>
-max_magnitude_diagonal(const Matrix& m){
-
-    typedef typename Matrix::size_type size_type;
-    typedef typename Matrix::size_type value_type;
-
-    const size_type n = m.size1();
+auto
+max_magnitude_diagonal(const Matrix& m)
+{
+    const int n = linalg::num_rows(m);
 
     if (n == 0)
-        return std::make_tuple(0, 0);
+        return std::make_tuple(0, 0.0);
 
-    size_type idx = 0;
-    value_type mmd = fabs(m(0, 0));
-    for (size_type i = 1; i < n; ++i){
-        const value_type mii = fabs(m(i, i));
+    int idx = 0;
+    auto mmd = fabs(m(0, 0));
+    for (int i = 1; i < n; ++i){
+        const auto mii = fabs(m(i, i));
         idx = mmd < mii ?  i : idx;
         mmd = idx == i ? mii : mmd;
     }
@@ -72,17 +71,14 @@ max_magnitude_diagonal(const Matrix& m){
 
 /// \brief Get the value of maximum magnitude off the diagonal of the matrix.
 template <typename Matrix>
-typename Matrix::value_type
-max_magnitude_off_diagonal(const Matrix& m){
+auto
+max_magnitude_off_diagonal(const Matrix& m)
+{
+    const int n = linalg::num_rows(m);
 
-    typedef typename Matrix::size_type size_type;
-    typedef typename Matrix::value_type value_type;
-
-    const size_type n = m.size1();
-
-    value_type mmd = fabs(m(0, 0));
-    for (size_type i = 0; i < n; ++i){
-        for (size_type j = i + 1; j < n; ++j){
+    auto mmd = fabs(m(0, 0));
+    for (int i = 0; i < n; ++i){
+        for (int j = i + 1; j < n; ++j){
             mmd = std::max(fabs(m(i, j)), mmd);
         }
     }
