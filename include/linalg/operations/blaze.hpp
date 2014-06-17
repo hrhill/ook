2bdbdd52b24caff241f4726cc5a455db614b7a73
harry <hill.harry.r@gmail.com>
2014-06-17 18:06:26 +0100

    Update norms.

--- include/linalg/operations/blaze.hpp
+++ include/linalg/operations/blaze.hpp
@@ -3,6 +3,8 @@
 
 #if HAVE_BLAZE
 
+#include "linalg/std_traits.hpp"
+
 #include <blaze/Math.h>
 
 namespace linalg{
@@ -84,6 +86,61 @@ swap(blaze::DenseRow<Matrix1<T1, SO1>>& r1, blaze::DenseRow<Matrix2<T2, SO2>>& r
     for (int i = 0; i < n; ++i)
         r2[i] = tmp[i];
 }
+
+/// \brief Inner product
+template <typename T, bool TF1, bool TF2>
+auto inner_prod(const blaze::DynamicVector<T, TF1>& x, const blaze::DynamicVector<T, TF2>& y)
+{
+    typedef remove_const_reference<decltype(x[0])> value_type;
+    assert(linalg::size(x) == linalg::size(y));
+    return std::inner_product(
+                x.begin(), x.end(), y.begin(), value_type(0.0));
+}
+
+/// \brief \f$ l_1 \f$ norm.
+template <typename T, bool TF>
+auto norm_1(const blaze::DynamicVector<T, TF>& x)
+{
+    typedef remove_const_reference<decltype(x[0])> value_type;
+    return std::accumulate(x.begin(), x.end(), value_type(0.0),
+            [](const value_type& init, const value_type& xi){
+                return init + fabs(xi);
+            });
+}
+
+/// \brief \f$ l_2  \f$ norm.
+template <typename T, bool TF>
+auto norm_2(const blaze::DynamicVector<T, TF>& x)
+{
+    return sqrt(inner_prod(x, x));
+}
+
+/// \brief \f$ l_p  \f$ norm.
+template <typename T, bool TF>
+auto norm_p(const blaze::DynamicVector<T, TF>& x, int p)
+{
+    assert(p > 0);
+    typedef remove_const_reference<decltype(x[0])> value_type;
+    value_type r(0.0);
+    for (const auto& xi : x)
+        r += std::pow(xi, p);
+    return exp(log(r)/p);
+}
+
+/// \brief \f$ l_{\infty} \f$ norm.
+template <typename T, bool TF>
+auto norm_infinity(const blaze::DynamicVector<T, TF>& x)
+{
+    typedef remove_const_reference<decltype(x[0])> value_type;
+    value_type r(0.0);
+    for (const auto& xi : x){
+        const value_type fxi = fabs(xi);
+        if (fxi > r)
+            r = fxi;
+    }
+    return r;
+}
+
 
 } // ns linalg
 
--- include/linalg/operations/ublas.hpp
+++ include/linalg/operations/ublas.hpp
@@ -1,6 +1,7 @@
 #ifndef LINALG_OPERATIONS_UBLAS_HPP_
 #define LINALG_OPERATIONS_UBLAS_HPP_
 
+#include <boost/numeric/ublas/vector_expression.hpp>
 #include <boost/numeric/ublas/matrix_expression.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
@@ -8,16 +9,15 @@
 #include <boost/numeric/ublas/vector_proxy.hpp>
 #include <boost/numeric/ublas/matrix_proxy.hpp>
 
+#include "linalg/std_traits.hpp"
+
 namespace linalg{
 
-template <
-    template<typename, typename> class V,
-    typename T,
-    typename A>
+template <typename Exp>
 size_t
-size(const V<T, A>& v)
+size(const boost::numeric::ublas::vector_expression<Exp>& x)
 {
-    return v.size();
+    return x().size();
 }
 
 template <typename MatrixExprT>
@@ -57,16 +57,73 @@ trans(const MatrixType& M)
 	return boost::numeric::ublas::trans(M);
 }
 
-template <typename Vector>
+template <typename T>
 auto
-outer_prod(const Vector& x, const Vector& y) -> decltype(boost::numeric::ublas::outer_prod(x, y))
+outer_prod(const boost::numeric::ublas::vector_expression<T>& x,
+           const boost::numeric::ublas::vector_expression<T>& y)
+    -> decltype(boost::numeric::ublas::outer_prod(x(), y()))
 {
-    return boost::numeric::ublas::outer_prod(x, y);
+    return boost::numeric::ublas::outer_prod(x(), y());
 }
 
 template <typename T, typename U>
 void swap(T& t, U& u){
     std::swap(t, u);
+}
+
+/// \brief Inner product
+template <typename T>
+auto inner_prod(const boost::numeric::ublas::vector_expression<T>& x,
+                const boost::numeric::ublas::vector_expression<T>& y)
+{
+    typedef remove_const_reference<decltype(x()[0])> value_type;
+    assert(linalg::size(x()) == linalg::size(y()));
+    return std::inner_product(
+                x().begin(), x().end(), y().begin(), value_type(0.0));
+}
+
+/// \brief \f$ l_1 \f$ norm.
+template <typename T>
+auto norm_1(const boost::numeric::ublas::vector_expression<T>& x)
+{
+    typedef remove_const_reference<decltype(x()[0])> value_type;
+    return std::accumulate(x().begin(), x().end(), value_type(0.0),
+            [](const value_type& init, const value_type& xi){
+                return init + fabs(xi);
+            });
+}
+
+/// \brief \f$ l_2  \f$ norm.
+template <typename T>
+auto norm_2(const boost::numeric::ublas::vector_expression<T>& x)
+{
+    return sqrt(inner_prod(x(), x()));
+}
+
+/// \brief \f$ l_p  \f$ norm.
+template <typename T>
+auto norm_p(const boost::numeric::ublas::vector_expression<T>& x, int p)
+{
+    assert(p > 0);
+    typedef remove_const_reference<decltype(x()[0])> value_type;
+    value_type r(0.0);
+    for (const auto& xi : x())
+        r += std::pow(xi, p);
+    return exp(log(r)/p);
+}
+
+/// \brief \f$ l_{\infty} \f$ norm.
+template <typename T>
+auto norm_infinity(const boost::numeric::ublas::vector_expression<T>& x)
+{
+    typedef remove_const_reference<decltype(x()[0])> value_type;
+    value_type r(0.0);
+    for (const auto& xi : x()){
+        const value_type fxi = fabs(xi);
+        if (fxi > r)
+            r = fxi;
+    }
+    return r;
 }
 
 } // ns linalg
