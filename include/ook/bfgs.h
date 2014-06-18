c8eb43bf3e543a3b12d4d15b6c4bcefff4c03d09
Harry Hill <harry.hill@smartodds.co.uk>
2014-06-18 16:11:22 +0100

    Update bfgs.

--- include/ook/bfgs.h
+++ include/ook/bfgs.h
@@ -19,10 +19,10 @@
 #define OOK_LINE_SEARCH_METHODS_BFGS_H_
 
 #include <tuple>
-#include <boost/numeric/ublas/matrix.hpp>
+
+#include "linalg.hpp"
 
 #include "ook/state.h"
-#include "ook/norms.h"
 #include "ook/line_search_method.h"
 #include "ook/line_search/more_thuente.h"
 
@@ -33,9 +33,9 @@ template <typename X>
 struct
 bfgs_state{
     typedef X vector_type;
-    typedef typename X::value_type value_type;
-    typedef boost::numeric::ublas::matrix<value_type,
-              boost::numeric::ublas::column_major> matrix_type;
+    typedef typename std::remove_reference<decltype(X()[0])>::type value_type;
+
+    typedef typename linalg::associated_matrix<X>::type matrix_type;
 
     bfgs_state(const int n = 0)
     :
@@ -73,14 +73,15 @@ bfgs_state{
 template <typename X>
 struct bfgs{
     typedef X vector_type;
-    typedef typename X::value_type value_type;
+    typedef typename std::remove_reference<decltype(X()[0])>::type value_type;
+    typedef typename linalg::associated_matrix<X>::type matrix_type;
     typedef bfgs_state<X> state_type;
 
     template <typename F>
     state_type
     initialise(F objective_function, const X& x0)
     {
-        state_type s(x0.size());
+        state_type s(linalg::size(x0));
         std::tie(s.fx, s.dfx) = objective_function(x0);
         s.dfx0 = s.dfx;
         return s;
@@ -90,30 +91,29 @@ struct bfgs{
     descent_direction(state_type s)
     {
         ++s.iteration;
-        s.p = -boost::numeric::ublas::prod(s.H, s.dfx);
+        linalg::gemv(-1.0, s.H, s.dfx, 0.0, s.p);
         return s;
     }
 
     state_type
     update(state_type s){
-        namespace ublas = boost::numeric::ublas;
-        typedef ublas::matrix<double, ublas::column_major> matrix_type;
-
         X y(s.dfx - s.dfx0);
-        const value_type rho = 1.0/inner_product(y, s.dx);
+        const value_type rho = 1.0/linalg::inner_prod(y, s.dx);
 
-        const int n = s.dfx.size();
-        matrix_type Z(ublas::identity_matrix<double>(n)
-                        - rho * ublas::outer_prod(s.dx, y));
-        matrix_type ss = rho * ublas::outer_prod(s.dx, s.dx);
+        const int n = linalg::size(s.dfx);
+        matrix_type Z(linalg::identity_matrix<matrix_type>(n) - rho * linalg::outer_prod(s.dx, y));
+        matrix_type ss = rho * linalg::outer_prod(s.dx, s.dx);
 
         if (s.iteration == 1){
-            const value_type hii = inner_product(s.dx, s.dx);
+            const value_type hii = linalg::inner_prod(s.dx, s.dx);
             for (int i = 0; i < n; ++i){
                 s.H(i, i) = hii;
             }
         }
-        s.H = ublas::prod(Z, matrix_type(ublas::prod(s.H, ublas::trans(Z)))) + ss;
+        matrix_type tmp(n, n);
+        linalg::gemm(1.0, s.H, linalg::trans(Z), 0.0, tmp);
+        linalg::gemm(1.0, Z, tmp, 0.0, s.H);
+        s.H += ss;
         s.dfx0 = s.dfx;
         return s;
     }
