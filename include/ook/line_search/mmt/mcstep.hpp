c5c905e19685568d81b1045fa40ac4f908adabf5
harry <hill.harry.r@gmail.com>
2014-08-04 22:28:03 +0100

    Update line search code.

--- include/ook/line_search/mmt/mcstep.hpp
+++ include/ook/line_search/mmt/mcstep.hpp
@@ -1,69 +1,11 @@
 #ifndef LBFGS_MCSTEP_HPP_
 #define LBFGS_MCSTEP_HPP_
 
-#include <boost/math/special_functions/sign.hpp>
+#include "ook/line_search/step_functions.hpp"
 
 namespace ook{
 namespace line_search{
 namespace mmt{
-
-/// \brief Calculate the secant step.
-/// \tparam T Numeric type, supporting operators *, / and -.
-/// \param x Point x.
-/// \param dx Derivative of function at x.
-/// \param y Point y.
-/// \param dy Derivative of function at y.
-template <typename T>
-T
-secant_step(T x, T dx, T y, T dy)
-{
-    return y - dy * (y - x) / (dy - dx);
-}
-
-/// \brief Calculate the value which minimizes the quadratic interpolant of
-/// the input values.
-/// \tparam T Numeric type, supporting operators +, *, / and -.
-/// \param x Point x.
-/// \param fx Function value at x.
-/// \param dx Derivative of function at x.
-/// \param y Point y.
-/// \param fy Function value at y.
-template <typename T>
-T
-quadratic_step(T x, T fx, T dx, T y, T fy)
-{
-    return x + dx / ((fx - fy) / (y - x) + dx) / 2.0 * (y - x);
-}
-
-/// \brief Calculate the value which minimizes the cubic interpolant of the
-/// input values. The function normalizes values in the square root.
-/// \tparam T Numeric type, supporting operators +, *, / and -.
-/// \param x Point x.
-/// \param fx Function value at x.
-/// \param dx Derivative of function at x.
-/// \param y Point y.
-/// \param fy Function value at y.
-/// \param dy Derivative of function at y.
-template <typename T>
-T
-cubic_step(T x, T fx, T dx, T y, T fy, T dy)
-{
-    using boost::math::sign;
-
-    const T d1 = dx + dy - 3 * (fx - fy)/(x - y);
-    const T s = std::max({fabs(d1), fabs(dx), fabs(dy)});
-    const T d2 = sign(y - x) * s * sqrt(pow(d1 / s, 2) - (dx / s) * (dy / s));
-    return y - (y - x) * (dy + d2 - d1)/(dy - dx + 2.0 * d2);
-}
-
-/// \brief Return true if a is closer to c than b is.
-/// \tparam T Numeric type, supporting operators +, *, / and -.
-template <typename T>
-bool
-is_closer(T a, T b, T c)
-{
-    return fabs(a - c) < fabs(b - c);
-}
 
 /// \brief The purpose of mcstep is to compute a safeguarded step for a
 /// linesearch and to update an interval of uncertainty for a minimizer of the
