25a060283863f8d2b6bbb0e1823e1e6c96184cb2
harry <hill.harry.r@gmail.com>
2014-08-24 19:02:23 +0100

    Update tests.

--- test/test_nonlinear_cg.cpp
+++ test/test_nonlinear_cg.cpp
@@ -1,14 +1,23 @@
-#define BOOST_TEST_MODULE fletcher_reeves
+#define BOOST_TEST_MODULE nonlinear_cg
 
 #include <boost/test/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
 #include <boost/test/test_tools.hpp>
 
-#include "ook/fletcher_reeves.hpp"
-
+#include <boost/mpl/list.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 
+#include "ook/nonlinear_cg.hpp"
+
 typedef boost::numeric::ublas::vector<double> vector_type;
+
+// List of beta update methods
+typedef boost::mpl::list<
+    ook::beta::fr
+    ,ook::beta::pr
+    ,ook::beta::hs
+    ,ook::beta::dy
+> betas;
 
 struct state
 {
@@ -18,38 +27,15 @@ struct state
     vector_type dfx;
 };
 
-BOOST_AUTO_TEST_CASE(fletcher_reeves_descent_direction)
+BOOST_AUTO_TEST_CASE_TEMPLATE(nonlinear_cg_descent_direction, T, betas)
 {
     // First step is a descent direction.
     state s;
     s.dfx[0] = 1.234;
     s.dfx[1] = 5.678;
-    ook::fletcher_reeves_impl<vector_type> scheme(s);
+    ook::nonlinear_cg_impl<vector_type, T> scheme(s);
     auto dd = scheme.descent_direction(s);
     BOOST_CHECK_EQUAL(dd[0], -s.dfx[0]);
     BOOST_CHECK_EQUAL(dd[1], -s.dfx[1]);
 }
 
-BOOST_AUTO_TEST_CASE(fletcher_reeves_update)
-{
-    // First step is a descent direction.
-    state s;
-    s.dfx[0] = 1;
-    s.dfx[1] = 1;
-    ook::fletcher_reeves_impl<vector_type> scheme(s);
-
-    // First step is descent direction
-    // stores p
-    auto dd = scheme.descent_direction(s);
-    scheme.update(s);
-
-    dd = scheme.descent_direction(s);
-    BOOST_CHECK_EQUAL(dd[0], -2.0);
-    BOOST_CHECK_EQUAL(dd[1], -2.0);
-    scheme.update(s);
-
-    dd = scheme.descent_direction(s);
-    BOOST_CHECK_EQUAL(dd[0], -3.0);
-    BOOST_CHECK_EQUAL(dd[1], -3.0);
-    scheme.update(s);
-}
