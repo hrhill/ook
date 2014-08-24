a8d8671467b7998a49cc7845a82fa45d76f6a1de
harry <hill.harry.r@gmail.com>
2014-08-24 19:00:54 +0100

    Add gc example.

--- include/ook/nonlinear_cg.hpp
+++ include/ook/nonlinear_cg.hpp
@@ -31,43 +31,50 @@ namespace ook{
 /// for Fletcher-Reeves method. The intitial step is a steepest descent
 /// step. However, subsequent steps use the previous descent direction
 /// in a linear combination with the steepest descent direction.
-
-struct beta_fr
+namespace beta{
+struct fr
 {
     template <typename X>
-    auto operator()(const X& dxp, const X& dxq, const X& p)
+    auto operator()(const X& dxp, const X& dxq, const X& p) const
+    -> remove_const_reference<decltype(dxp[0])>
     {
         return linalg::inner_prod(dxp, dxp)/linalg::inner_prod(dxq, dxq);
     }
 };
 
-struct beta_pr
+struct pr
 {
     template <typename X>
-    auto operator()(const X& dxp, const X& dxq, const X& p)
+    auto operator()(const X& dxp, const X& dxq, const X& p) const
+    -> remove_const_reference<decltype(dxp[0])>
     {
-        return linalg::inner_prod(dxp, dxp - dxq)/linalg::inner_prod(dxq, dxq);
+        return linalg::inner_prod(dxp,
+                static_cast<const X&>(dxp - dxq))/linalg::inner_prod(dxq, dxq);
     }
 };
 
-struct beta_hs
+struct hs
 {
     template <typename X>
-    auto operator()(const X& dxp, const X& dxq, const X& p)
+    auto operator()(const X& dxp, const X& dxq, const X& p) const
+    -> remove_const_reference<decltype(dxp[0])>
     {
-        return linalg::inner_prod(dxp, dxp - dxq)/linalg::inner_prod(p, dxp - dxq);
+        X d = dxp - dxq;
+        return linalg::inner_prod(dxp, d)/linalg::inner_prod(p, d);
     }
 };
 
-struct beta_dy
+struct dy
 {
     template <typename X>
-    auto operator()(const X& dxp, const X& dxq, const X& p)
+    auto operator()(const X& dxp, const X& dxq, const X& p) const
+    -> remove_const_reference<decltype(dxp[0])>
     {
-        return linalg::inner_prod(dxp, dxp)/linalg::inner_prod(p, dxp - dxq);
+        return linalg::inner_prod(dxp, dxp)/linalg::inner_prod(p,
+                    static_cast<const X&>(dxp - dxq));
     }
 };
-
+} // ns beta
 
 template <typename X, typename Beta>
 struct nonlinear_cg_impl
@@ -87,7 +94,7 @@ struct nonlinear_cg_impl
     {}
 
     /// \brief Calculate and return the descent direction given by
-    ///
+    /// \f[ p_{k+1} = -\nabla f(x_k) + \beta_k p_k\f]
     /// \tparam State The obtimiser state type.
     /// \param s The current state.
     /// \return The descent direction.
@@ -99,15 +106,16 @@ struct nonlinear_cg_impl
         return p;
     }
 
-    /// \brief Update \f$\beta\f$, reseting after n steps.
+    /// \brief Update \f$\beta\f$ and \f$\nabla f(x)\f$.
+    /// \tparam State The obtimiser state type.
+    /// \param s The current state.
     template <typename State>
     void
     update(const State& s)
     {
         const int n = dfx.size();
-        beta = (iter % n) ? beta_fun(s.dfx, dfx, p) : 0;
+        beta = beta_fun(s.dfx, dfx, p);
         dfx = s.dfx;
-        ++iter;
     }
 
 private:
@@ -121,12 +129,45 @@ private:
 /// \brief The Fletcher-Reeves algorithm.
 template <typename F, typename X, typename Options, typename Observer>
 std::tuple<ook::message, X>
-nonlinear_cg(F f, const X& x0, const Options& opts, Observer& observer)
+fletcher_reeves(F f, const X& x0, const Options& opts, Observer& observer)
 {
-    typedef nonlinear_cg_impl<X> scheme;
+    typedef nonlinear_cg_impl<X, beta::fr> scheme;
     line_search_method<scheme> method;
     return method(f, x0, opts, observer);
 }
+
+/// \brief The Polak-Ribiere algorithm.
+template <typename F, typename X, typename Options, typename Observer>
+std::tuple<ook::message, X>
+polak_ribiere(F f, const X& x0, const Options& opts, Observer& observer)
+{
+    typedef nonlinear_cg_impl<X, beta::pr> scheme;
+    line_search_method<scheme> method;
+    return method(f, x0, opts, observer);
+}
+
+/// \brief The Hestenes-Steifel algorithm.
+template <typename F, typename X, typename Options, typename Observer>
+std::tuple<ook::message, X>
+hestenes_steifel(F f, const X& x0, const Options& opts, Observer& observer)
+{
+    typedef nonlinear_cg_impl<X, beta::hs> scheme;
+    line_search_method<scheme> method;
+    return method(f, x0, opts, observer);
+}
+
+/// \brief The Dai-Yuan algorithm.
+template <typename F, typename X, typename Options, typename Observer>
+std::tuple<ook::message, X>
+dai_yuan(F f, const X& x0, const Options& opts, Observer& observer)
+{
+    typedef nonlinear_cg_impl<X, beta::dy> scheme;
+    line_search_method<scheme> method;
+    return method(f, x0, opts, observer);
+}
+
+
+
 } //ns ook
 
 #endif
