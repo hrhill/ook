7407ec2a7dd2f45308befbfcfcfd18a12eb02aab
harry <hill.harry.r@gmail.com>
2014-08-24 16:48:22 +0100

    Add line search opts.

--- include/ook/options.hpp
+++ include/ook/options.hpp
@@ -21,40 +21,29 @@
 #include <cassert>
 #include <limits>
 
+#include "ook/line_search/options.hpp"
+
 namespace ook{
 
 template <typename T>
-struct options
+struct options : line_search::options<T>
 {
     options()
     :
-        ftol(T(1e-03)),
-        gtol(T(9e-01)),
-        stpmin(std::numeric_limits<T>::epsilon()),
-        stpmax(T(5.0)),
-        maxfev(20)
+        eps(T(1e-05)),
+        max_iter(1000)
     {}
-
-    options(T ftol, T gtol, T stpmin, T stpmax)
+/*
+    options(T eps, uint maxiter)
     :
         ftol(ftol), gtol(gtol),  stpmin(stpmin), stpmax(stpmax)
     {
-        assert(ftol > T(0) && "ftol <= 0.0");
-        assert(gtol > T(0) && "gtol <= 0.0");
-        assert(stpmin >= T(0.0) && "stpmin < 0.0");
-        assert(stpmax > stpmin && "stpmax <= stpmin.");
     }
-
-    // tolerance for the sufficient decrease condition.
-    T ftol;
-    // tolerance for the curvature condition.
-    T gtol;
-    // lower bound for the step.
-    T stpmin;
-    // upper bound for the step.
-    T stpmax;
-
-    uint maxfev;
+*/
+    // tolerance for the first order convergence criteria
+    T eps;
+    // maximum number of iterations
+    uint max_iter;
 };
 
 } // ns ook
